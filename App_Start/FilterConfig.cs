20ef8a2d277a177a50d11959bad4937d413160bc
siamt <siamt@SIAM>
2025-04-12 16:20:30 +0600

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Learning01ADN
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Learning01ADN.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome(string name, int ID=1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + " Your ID is " + ID);
+        }
+        public ActionResult TestView()
+        {
+            return View();
+        }
+        public ActionResult ErrorMessage()
+        {
+            return View();
+        }
+
+        public string PrintMessage()
+        {
+            return "<h1>Welcome to my world!</h1><p>This is just a test paragraph</p>";
+        }
+    }
+}
